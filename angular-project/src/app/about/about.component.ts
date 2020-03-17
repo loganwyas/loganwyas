6e9798e0b524464c27097e59b42b2dab827caccd
loganwyas <loganw9999@gmail.com>
2020-03-17 18:39:38 -0500

    Work from 03/11/20

--- angular-project/src/app/about/about.component.ts
+++ angular-project/src/app/about/about.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
   <div class="container text-center">
     <h3>About the Company</h3>
-    <p style="text-align: left">This company strives to give you the best results. We have been around for {{yearsAgo}} years, so we know what we are doing. Although {{founded}} was a rough year, we were able to rise to the occasion and build our business from the ground.</p>
+    <p style="text-align: left">This company strives to give you the best results. We have been around for {{yearsAgo}} years, so we know what we are doing. Although {{founded}} was a rough year, we were able to rise to the occasion and build our business from the ground up.</p>
   </div>
   `
 })
